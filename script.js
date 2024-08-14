6a7369866a98a2205c36806bf7a13469ad3f7a41
alexitrico14 <alex.villagran61@gmail.com>
2024-08-14 19:43:31 -0400

    Tablas de multiplicar y Factorial del numero

---
+++ script.js
@@ -0,0 +1,36 @@
+// Funcion para: Multiplicarse hasta el numero ingresado (por el usuario) y el factorial de ese numero
+function multiplicar() {
+    // Solicitar Numero al usuario
+    let num = parseInt(prompt("Ingresa el primer número entre 1 a 20"));
+
+    // Validar que el numero sea entre 1 y 20
+    if (num < 1 || num > 20) {
+
+        console.log("Numero fuera de rango. Ingresar entre 1 a 20");
+        return false;
+    }
+    // Tabla multiplicar
+    for (let i = 1; i <= num; i++) {
+
+        console.log(`${num} x ${i} = ${num * i}`);
+        // document.write(`${num} x ${i} = ${num * i} </br>`);
+    }
+
+    // Factorial
+    for (let i = 1; i <= num; i++) {
+
+        let factorial = 1;
+
+        for (let j = 1; j <= i; j++) {
+
+            factorial = factorial * j;
+
+        }
+
+        console.log(`Factorial de ${i} es: ${factorial}`);
+        // document.write(`Factorial de ${i} es: ${factorial} </br>`);
+    }
+}
+
+// Llamar a la funcion
+multiplicar()
